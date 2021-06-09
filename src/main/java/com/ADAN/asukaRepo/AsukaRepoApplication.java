0530cba2da2273a69d9d177bb5e7da01f7d78393
mrtnmmn <mrtn.lmba@gmail.com>
2021-06-10 00:14:45 +0200

    first commit

---
+++ src/main/java/com/ADAN/asukaRepo/AsukaRepoApplication.java
@@ -0,0 +1,13 @@
+package com.ADAN.asukaRepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsukaRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsukaRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ADAN/asukaRepo/AsukaRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ADAN.asukaRepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsukaRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
